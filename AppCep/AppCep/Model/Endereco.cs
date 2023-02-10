6f946085d4532b78c2d4d7f2af0c6f47451965c1
Usuario <Usuario@CPD02-M09>
2023-02-10 14:36:20 -0300

    inserindo as classes

---
+++ AppCep/AppCep/Model/Endereco.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCep.Model
+{
+    public class Endereco
+    {
+        public int id_endereco { get; set; }
+    }
+}
